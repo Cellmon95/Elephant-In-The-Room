1ba3568122c0bf6d49994daa68a25e66006824d3
Lucas Källberg <lucas.arne@hotmail.com>
2022-10-21 15:30:18 +0200

    Added license, folders and files

--- pages/room1.php
+++ pages/room1.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php
-
-?>
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>The Elephant In The Room</title>
 </head>
 
 <body>
+    <form action="/action_page.php">
+        <label for="fname">First name:</label>
+        <input type="text" id="fname" name="fname"><br><br>
 
+    </form>
 </body>
 
 </html>
---
+++ php/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function isAntsMoreThanElephants(int $ants, int $elephants): bool
+{
+    if ($ants > $elephants)
+        return true;
+    else
+        return false;
+}
